d9c23d4d027e884627bad9d3ef3e579ccf293e78
yarnaid <yarnaid@gmail.com>
2015-04-04 15:50:02 +0300

    coloring and facet grid

--- server.R
+++ server.R
@@ -4,32 +4,61 @@
 #
 # http://shiny.rstudio.com
 #
+require(ggplot2)
+
 
 library(shiny)
 library(ggplot2)
 
 # data <- WDI(extra = TRUE, country = c("US","CA","MX"))
 data <- diamonds
+points_fill <- I("darkorchid1")
+alpha = 0.1
 
-plotType <- function(shape=1, type, binwidth=5) {
+plotType <- function(shape=1, type, binwidth=5, pch=21, alpha=0.1) {
   switch(type,
-         sp = geom_point(shape=shape),
-         hp = geom_histogram(shape=shape, binwidth=binwidth),
-         p = pie())
+         sp = geom_point(shape=shape, pch = pch),
+         hp = geom_histogram(binwidth=binwidth),
+#          p = pie()
+         )
 }
 
 shinyServer(function(input, output) {
+  input_data <- reactive({
+    data[, c(input$column_x, input$column_y, input$column_facet, input$column_color, 'color')]
+    })
+  
+  output$table_view <- renderDataTable({
+    data
+  })
+  
+  
   output$main_plot <- renderPlot({
+    res <- ggplot(data, aes_string(x=input$column_x), environment = environment())
     if (input$plot_type == "hp") {
-      x <- data[input$column_x]
+      x <- input_data()[input$column_x]
       binwidth <- diff(range(x)) / input$bins
-      res <- ggplot(data, aes_string(x=input$column_x)) + plotType(binwidth=binwidth, type=input$plot_type)
+      res <- res + geom_histogram(binwidth=binwidth)
     } else {
-      res <- ggplot(data, aes_string(x=input$column_x, y=input$column_y)) + plotType(type=input$plot_type)
+      res <- res + plotType(type=input$plot_type) + aes_string(y=input$column_y)
     }
+    
+    if (input$facet_grid == TRUE) {
+      f <- as.formula(paste(input$column_facet, '~ .'))
+      res <- res + facet_grid(f)
+    }
+    
+    if (input$coloring == TRUE) {
+      res <- res + aes(color=factor(get(input$column_color)))
+    }
+    
+    if (FALSE) {
+      res <- res + geom_jitter(alpha = alpha)
+    }
+    
     print(res)
     })
-})
+  })
 
 # shinyServer(function(input, output) {
 # 
--- ui.R
+++ ui.R
@@ -5,42 +5,97 @@
 # http://shiny.rstudio.com
 #
 
+require(ggplot2)
+require(shinydashboard)
+
 library(shiny)
 library(shinydashboard)
-library(WDI)
+library(ggplot2)
 
-data <- WDI(extra = TRUE, country = c("US","CA","MX"))
+# library(WDI)
+
+# data <- WDI(extra = TRUE, country = c("US","CA","MX"))
+data <- diamonds
 cols <- c(colnames(data))
+plot_width <- 9
+full_width <- 12
+plot_height <- 600
+side_bar <- TRUE
+
+dashboard_sidebar <- dashboardSidebar(
+  disable = !side_bar,
+  sidebarMenu(
+    menuItem("Main", tabName="main"),
+    menuItem("Data Table", tabName="data_table")
+  )
+)
+
+main_controls_box <- box(
+  title = "Controls",
+  width = full_width - plot_width,
+  selectInput(
+    "plot_type",
+    "Plot Type",
+    list("sp", "hp")
+  ),
+  selectInput(
+    "column_x",
+    "X values",
+    cols,
+    #         inline = TRUE
+  ),
+  selectInput(
+    "column_y",
+    "Y values",
+    cols,
+    #         inline = TRUE
+  ),
+  numericInput('bins', 'Histogram bins', 30, min = 1),
+  checkboxInput('facet_grid', 'Facet grid'),
+  selectInput(
+    "column_facet",
+    "Facet grid",
+    cols,
+    #         inline = TRUE
+  ),
+  checkboxInput('coloring', 'Color'),
+  selectInput(
+    "column_color",
+    "Colors for",
+    cols,
+    #         inline = TRUE
+  )
+)
+
+main_tab_item <- tabItem(
+  tabName = 'main',
+  fluidRow(
+    box(plotOutput("main_plot", height = plot_height), width = plot_width),
+    main_controls_box
+  )
+)
+
+data_table_tab_item <- tabItem(
+  tabName = 'data_table',
+  h2('Data'),
+  fluidRow(
+    box(dataTableOutput('table_view'), width=12)
+  )
+)
+
+dashboard_body <- dashboardBody(
+  tabItems(
+    main_tab_item,
+    data_table_tab_item
+  )
+)
+
 
 ui <- dashboardPage(
   dashboardHeader(title = "ggplot"),
-  dashboardSidebar(disable = TRUE),
-  dashboardBody(
-    fluidRow(
-    box(plotOutput("main_plot", height = 400)),
-    box(
-      title = "Controls",
-      selectInput(
-        "plot_type",
-        "Plot Type",
-        list("sp", "hp", "p")
-        ),
-      selectInput(
-        "column_x",
-        "X values",
-        cols,
-#         inline = TRUE
-      ),
-      selectInput(
-        "column_y",
-        "Y values",
-        cols,
-#         inline = TRUE
-      ),
-      numericInput('bins', 'Histogram bins', 30, min = 1)
-    )
-    ))
-  )
+  dashboard_sidebar,
+  dashboard_body
+)
 
 # shinyUI(fluidPage(
 # 
